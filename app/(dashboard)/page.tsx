3952df553f870e91e8fcc0df7468de03ddfdce76
Shubham32142 <shubhamdubey23042004@gmail.com>
2025-07-26 20:15:49 +0530

    feat: add initial components and navigation structure
    - Created components for header, navigation, and buttons.
    - Implemented responsive design for navigation using sheets for mobile.
    - Added welcome message component to greet users.
    - Integrated user authentication with Clerk.
    - Established database connection using Drizzle ORM and Neon.
    - Defined database schema for accounts.
    - Added API hook to fetch accounts data.
    - Updated package.json with new dependencies for Clerk, Drizzle, and React Query.
    - Removed unused SVG files and added a new logo SVG.

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+
+export default function Home() {
+  const accountsQuery = useGetAccounts();
+  return (
+    <div>
+      {accountsQuery.data?.map((account) => (
+        <div key={account.id}>{account.name}</div>
+      ))}
+    </div>
+  );
+}
