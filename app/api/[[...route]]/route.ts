88c65d59362a86eb2128a7120eb1262383be500f
Shubham32142 <shubhamdubey23042004@gmail.com>
2025-10-03 10:04:36 +0530

    feat: add categories management with CRUD operations and UI components

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import accounts from "./accounts";
+import categories from "./categories";
 import { HTTPException } from "hono/http-exception";
 export const runtime = "edge";
 
@@ -13,7 +14,9 @@ app.onError((err, c) => {
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const routes = app.route("/accounts", accounts);
+const routes = app
+  .route("/accounts", accounts)
+  .route("/categories", categories);
 
 export const GET = handle(app);
 export const POST = handle(app);
